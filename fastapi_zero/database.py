328d3239778db04184f1df4ccba2441265f8da4a
Francisco Ferreira <franciscobusiness3007@gmail.com>
2026-02-25 00:54:03 -0300

    Fix: Forçando driver asyncpg no database.py

--- fastapi_zero/database.py
+++ fastapi_zero/database.py
@@ -2,7 +2,14 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from fastapi_zero.settings import Settings
 
-engine = create_async_engine(Settings().DATABASE_URL)
+settings = Settings()
+
+if settings.DATABASE_URL.startswith('postgres://'):
+    settings.DATABASE_URL = settings.DATABASE_URL.replace(
+        'postgres://', 'postgresql+asyncpg://', 1
+    )
+
+engine = create_async_engine(settings.DATABASE_URL)
 
 
 async def get_session():  # pragma: no cover
